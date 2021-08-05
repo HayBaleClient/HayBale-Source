2d18be56082a158fdbfb45cbec4a818c8a2f58ad
NudTix <72350452+NudTix@users.noreply.github.com>
2021-08-05 20:55:30 +0300

    Add files via upload

---
+++ src/java/me/luxtix/haybale/event/events/ChatEvent.java
@@ -0,0 +1,19 @@
+package me.luxtix.haybale.event.events;
+
+import me.luxtix.haybale.event.EventStage;
+import net.minecraftforge.fml.common.eventhandler.Cancelable;
+
+@Cancelable
+public class ChatEvent
+        extends EventStage {
+    private final String msg;
+
+    public ChatEvent(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return this.msg;
+    }
+}
+
