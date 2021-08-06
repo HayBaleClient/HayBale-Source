6afbdd9e25ee606dc8a9f03cddd03bb729cdb1b6
NudTix <72350452+NudTix@users.noreply.github.com>
2021-08-06 16:53:52 +0300

    Add files via upload

---
+++ src/main/java/me/luxtix/haybale/manager/NotificationManager.java
@@ -0,0 +1,27 @@
+package me.luxtix.haybale.manager;
+
+import me.luxtix.haybale.Phobos;
+import me.luxtix.haybale.features.modules.client.HUD;
+import me.luxtix.haybale.features.notifications.Notifications;
+
+import java.util.ArrayList;
+
+public class NotificationManager {
+    private final ArrayList<Notifications> notifications = new ArrayList();
+
+    public void handleNotifications(int posY) {
+        for (int i = 0; i < this.getNotifications().size(); ++i) {
+            this.getNotifications().get(i).onDraw(posY);
+            posY -= Phobos.moduleManager.getModuleByClass(HUD.class).renderer.getFontHeight() + 5;
+        }
+    }
+
+    public void addNotification(String text, long duration) {
+        this.getNotifications().add(new Notifications(text, duration));
+    }
+
+    public ArrayList<Notifications> getNotifications() {
+        return this.notifications;
+    }
+}
+
